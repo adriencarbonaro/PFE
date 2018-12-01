fcf14b726aca2bd29878d3f4ed3d9d5bb9c446f4
RPi Weight Sensor <carbonaro.adrien@gmail.com>
2018-12-01 19:59:56 +0100

    Add new library to use HX711 with Python

---
+++ HX711_python_gandhalf/HX711_Python3/example_calibration.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+from hx711 import HX711		# import the class HX711
+import RPi.GPIO as GPIO		# import GPIO
+
+try:
+    # Create an object hx which represents your real hx711 chip
+    # Required input parameters are only 'dout_pin' and 'pd_sck_pin'
+    # If you do not pass any argument 'gain_channel_A' then the default value is 128
+    # If you do not pass any argument 'set_channel' then the default value is 'A'
+    # you can set a gain for channel A even though you want to currently select channel B
+    hx = HX711(dout_pin=21, pd_sck_pin=20, gain_channel_A=64)
+    cpt = 0	
+    result = hx.reset()		# Before we start, reset the hx711 ( not necessary)
+    if result:			# you can check if the reset was successful
+            print('Ready to use')
+    else:
+            print('not ready')
+    
+    # measure tare and save the value as offset for current channel
+    # and gain selected.
+    result = hx.zero(times=10)
+    hx.set_offset(-14050)
+    while(cpt < 1000):
+        raw = hx.get_raw_data_mean()
+        dat = hx.get_data_mean()
+        wei = hx.get_weight_mean()
+        print(str(raw))
+        print(str(dat))
+        print(str(wei) + " g")
+        print()
+        cpt += 1
+	
+except (KeyboardInterrupt, SystemExit):
+	print('Bye :)')
+	
+finally:
+	GPIO.cleanup()
+
