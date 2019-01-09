eb2c1936a7059158caf258998fc9e9c338e6238b
RPi Weight Sensor <carbonaro.adrien@gmail.com>
2019-01-09 11:22:19 +0100

    test.py: simple weight test

--- test.py
+++ test.py
@@ -2,9 +2,10 @@
 
 import sys
 import os
-import RPi.GPIO as     GPIO     # import GPIO
-from   time     import sleep
-
+import RPi.GPIO     as     GPIO     # import GPIO
+from   time         import sleep
+import pickle
+from   calibration  import calibrate
 # Add HX711 directory to sys.path.
 # Allows to find module hx711.
 #
@@ -12,7 +13,7 @@ from   time     import sleep
 # os.path.dirname()         = ~/PFE/
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from HX_mine.HX711_Python3.hx711 import HX711    # import the class HX711
+from HX711.HX711_Python3.hx711 import HX711    # import the class HX711
 
 try:
     # Create an object hx which represents your real hx711 chip
@@ -20,33 +21,25 @@ try:
     # If you do not pass any argument 'gain_channel_A' then the default value is 128
     # If you do not pass any argument 'set_channel' then the default value is 'A'
     # you can set a gain for channel A even though you want to currently select channel B
+    GPIO.setmode(GPIO.BCM)
     hx = HX711(dout_pin=21, pd_sck_pin=20, gain_channel_A=64)
-    cpt = 0
-    result = hx.reset()         # Before we start, reset the hx711
-
-    if result:
-        print('Ready to use')
+    # Check if we have swap file. If yes that suggest that the program was not
+    # terminated proprly (power failure). We load the latest state.
+    swap_file_name = 'swap_file.swp'
+    if os.path.isfile(swap_file_name):
+        with open(swap_file_name, 'rb') as swap_file:
+            # now we loaded the state before the Pi restarted.
+            hx = pickle.load(swap_file)
     else:
-        print('not ready')
-
-    # Measure tare and save the value as offset for current channel and gain selected.
-    result = hx.zero(readings=10)
-    print("zero value : " + str(hx.get_data_mean(10)))
-
-    # Calibrate using a known 50g weight.
-    input('Put calibrating weight (60g) on the scale the press Enter')
-    weight_60grams = hx.get_data_mean(10)
-    print("60g value  : " + str(weight_60grams))
-    hx.set_scale_ratio(scale_ratio=weight_60grams / 60.0)
-
-    # Read and print the current weight put on the scale.
+        hx = calibrate()
+    
     while(1):
         val = hx.get_weight_mean(10)
         print(str(val) + " g")
         sleep(1)
 
 except (KeyboardInterrupt, SystemExit):
-    print('Bye :)')
+    print('\nBye :)')
 
 finally:
     GPIO.cleanup()
