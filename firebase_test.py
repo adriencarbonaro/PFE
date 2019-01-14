7b94691989e360ac65beee259b6d7bae69177f79
RPi Weight Sensor <carbonaro.adrien@gmail.com>
2019-01-14 15:38:20 +0100

    WIP: add firebase_utils.py, firebase_test.py and API json file

---
+++ firebase_test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import RPi.GPIO     as     GPIO     # import GPIO
+from   time         import sleep
+import pickle
+from   calibration  import calibrate
+from   hx_loader    import hx_load
+from   firebase     import firebase_connect
+from   firebase     import send_notification
+
+# Add HX711 directory to sys.path.
+# Allows to find module hx711.
+#
+# os.path.abspath(__file__) = ~/PFE/example_calibration.py
+# os.path.dirname()         = ~/PFE/
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from HX711.HX711_Python3.hx711 import HX711    # import the class HX711
+
+# -- HX OBJECT LOADING ---------------------------------------------------------
+try:
+    hx = hx_load()
+
+# -- MAIN EXECUTION ------------------------------------------------------------
+
+    GPIO.setmode(GPIO.BCM)
+    val = hx.get_weight_mean(10)
+    print(str(val) + " g")
+    firebase_connect()
+    send_notification('Weight', str(val) + ' g')
+
+except (KeyboardInterrupt, SystemExit):
+    print('\ntest: Keyboard Interruption: stopping program')
+
+except (RuntimeError):
+    print("\ntest: Runtime Error during execution")
+
+except Exception as e:
+    print("\ntest: Other exception: " + e)
+
+finally:
+    GPIO.cleanup()
+
---
+++ firebase_utils.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import messaging
+
+# This registration token comes from the client FCM SDKs.
+registration_token = 'dTahG-Pwjf4:APA91bFMd7bSWwO0EBHrOl3nunkWNqrITzBUTY-FUOicWWouEO5BuvKSz-Y2haRG5GazGYCp6YPAA6ucs2RmBhW3WEPNlX2gwCbjBemghGD-SLpXfdly-yCFWPU-ObuXzBA21--xjeyC'
+
+##
+# @brief    Connect to Firebase Admin API.
+#           Necessary to send notifications.
+def firebase_connect():
+    cred = credentials.Certificate('./firebase_admin_sdk.json')
+    default_app = firebase_admin.initialize_app(cred)
+
+##
+# @brief        Send a Notification.
+#
+# @param title  The notification headline.
+# @param body   The notification message.
+def send_notification(notif_title='default_title', notif_body='default_body'):
+    # See documentation on defining a message payload.
+    message = messaging.Message(
+        notification=messaging.Notification(
+            title=notif_title,
+            body=notif_body,
+        ),
+        token=registration_token
+    )
+
+    response = messaging.send(message)
+    
+    print('Successfully sent message:', response)
