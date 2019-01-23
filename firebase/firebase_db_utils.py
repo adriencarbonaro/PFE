2a4c479c31971a49aa9eb83b2d5b95743d92910d
RPi Weight Sensor <carbonaro.adrien@gmail.com>
2019-01-23 15:35:12 +0100

    firebase_db_utils: update add_weight() function

--- firebase/firebase_db_utils.py
+++ firebase/firebase_db_utils.py
@@ -8,11 +8,20 @@ from    firebase_admin import db
 
 # -- API FUNCTIONS -------------------------------------------------------------
 
-def add_weight(id_weight, id_card, timestamp_weight, value):
+def add_weight(id_card, timestamp_weight, value):
+    # Get Firebase Realtime Database.
     weights_db = db.reference("/weights")
+    weights_t = get_weights()
+    
+    # Get last id_weight to increment it.
+    last_id_weight = 0
+    for i in weights_t:
+        if (weights_t[i]["id_weight"] > last_id_weight):
+            last_id_weight = weights_t[i]["id_weight"]
 
+    # Push data to Firebase Realtime Database.
     weights_db.push({
-        "id_weight": id_weight,
+        "id_weight": (last_id_weight + 1),
         "id_card": id_card,
         "timestamp_weight": timestamp_weight,
         "value": value
